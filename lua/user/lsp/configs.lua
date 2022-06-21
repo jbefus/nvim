eb1292aec4cf03cd53a0989331003e09da247894
Jurij Befus <jurij.befus@gmail.com>
2022-06-22 01:58:12 +0200

    added dockerls

--- lua/user/lsp/configs.lua
+++ lua/user/lsp/configs.lua
@@ -5,7 +5,7 @@ end
 
 local lspconfig = require("lspconfig")
 
-local servers = { "omnisharp", "tailwindcss", "tsserver", "marksman", "svelte", "jsonls", "sumneko_lua" }
+local servers = { "dockerls", "omnisharp", "tailwindcss", "tsserver", "marksman", "svelte", "jsonls", "sumneko_lua" }
 
 lsp_installer.setup({
 	ensure_installed = servers,
