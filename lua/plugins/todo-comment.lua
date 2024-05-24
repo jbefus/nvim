fb590db4073bec96cdd01e48809a10c3cca36270
Jurij Befus <jurij.befus@gmail.com>
2024-05-24 16:17:35 +0200

    add todo-comment config, add DRGG Symbol

---
+++ lua/plugins/todo-comment.lua
@@ -0,0 +1,10 @@
+-- DRGG:
+return {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {
+        keywords = {
+            DRGG = { icon = "󰩂", color = "hint" },
+        },
+    },
+}
