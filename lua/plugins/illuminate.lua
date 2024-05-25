6569575edabd7ab0ec0064f781533e1f19ea8144
Jurij Befus <jurij.befus@gmail.com>
2024-05-26 01:32:32 +0200

    disable illuminate, still does the highlighting stuff

--- lua/plugins/illuminate.lua
+++ lua/plugins/illuminate.lua
@@ -1,11 +1,11 @@
 return {
-    "RRethy/vim-illuminate",
-    opts = {
-        providers = { "lsp", "treesitter", "regex" },
-        delay = 100,
-        large_file_cutoff = 2000,
-        large_file_overrides = {
-            providers = { "lsp" },
-        },
-    },
+    -- "RRethy/vim-illuminate",
+    -- opts = {
+    --     providers = { "lsp", "treesitter", "regex" },
+    --     delay = 100,
+    --     large_file_cutoff = 2000,
+    --     large_file_overrides = {
+    --         providers = { "lsp" },
+    --     },
+    -- },
 }
