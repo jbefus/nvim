c9169fbdb2d0ee616f3562472c75d1181c41081f
Jurij Befus <jurij.befus@gmail.com>
2024-06-03 12:05:20 +0200

    disable autopairs

---
+++ lua/plugins/disabled-plugins.lua
@@ -0,0 +1,14 @@
+--
+-- every spec file under the "plugins" directory will be loaded automatically by lazy.nvim
+--
+-- In your by plugin files, you can:
+-- * add extra plugins
+-- * disable/enabled LazyVim plugins
+-- * override the configuration of LazyVim plugins
+return {
+
+    -- disable flash
+    -- { "folke/flash.nvim", enabled = false },
+    -- disable autopairs
+    { "echasnovski/mini.pairs", enabled = false },
+}
